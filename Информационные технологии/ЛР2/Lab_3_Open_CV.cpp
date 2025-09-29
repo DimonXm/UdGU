4fa61305a64028b7bbb33c49248869d19f955c4a
DimonXm <dmensikov110@gmail.com>
2025-09-29 09:23:35 +0400

    First commit

---
+++ Информационные технологии/ЛР2/Lab_3_Open_CV.cpp
@@ -0,0 +1,44 @@
+﻿#include "opencv2/imgcodecs.hpp"
+#include "opencv2/highgui.hpp"
+#include "opencv2/imgproc.hpp"
+#include <iostream>
+
+
+using namespace std;
+using namespace cv;
+
+int main()
+{
+    setlocale(LC_ALL, "");
+    Mat img,img2, medianImg, gaussImg, bilat, box;
+    //img = imread("image.jpg");
+    img = imread("12.jpg");
+    if (!img.empty())
+    {
+        imshow("Оригинал", img); // 3,4,5,9
+        GaussianBlur(img, gaussImg, Size(9, 9), 0);
+        medianBlur(img, medianImg, 7); // 2,6 (medianBlur(img, medianImg, 11)), 7, 11 (medianBlur(img, medianImg, 25)), 12
+        bilateralFilter(img, bilat, 3, 150, 150);
+        //imwrite("medianImg.jpg", bilat); // сохраняем измененное изображение
+        //img2 = imread("medianImg.jpg"); // считываем его в переменную
+        boxFilter(img2, box,0, Size(3, 3)); // 8, 12
+        imshow("Двусторонний фильтр", bilat);
+        imshow("Медианный фильтр", medianImg);
+        imshow("Фильтр гаусса", gaussImg); // 10
+        imshow("Фильтр Box", box); 
+        waitKey();
+    }
+    else
+        cout << "Файл изображения не найден!!!";
+}
+
+// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
+// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+
+// Советы по началу работы 
+//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
+//   2. В окне Team Explorer можно подключиться к системе управления версиями.
+//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
+//   4. В окне "Список ошибок" можно просматривать ошибки.
+//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
+//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
--- Информационные технологии/ЛР3/LaboratoryWork3.cpp
+++ Информационные технологии/ЛР3/LaboratoryWork3.cpp
@@ -2,25 +2,29 @@
 #include "opencv2/highgui.hpp"
 #include "opencv2/imgproc.hpp"
 #include <iostream>
+#include <string>
 
 using namespace cv;
+using namespace std;
 
 int main()
 {
     Mat HSVImage, outIsRangeImage, img;
-    img = imread("variant9.jpg"); // Считываем изображени
+    img = imread("image3.jpg"); // Считываем изображени
     int hMin = 9, hMax = 26; // Получаем из кода мамрыкина данные об HSV на картинке
     int sMin = 158, sMax = 255;
     int vMin = 226, vMax = 255;
 
-    cvtColor(img, HSVImage, COLOR_BGR2HSV);
-    inRange(HSVImage, Scalar(hMin, sMin, vMin), Scalar(hMax, sMax, vMax), outIsRangeImage);
+    cvtColor(img, HSVImage, COLOR_BGR2HSV); // Переводим изображение в hsv формат 
+    inRange(HSVImage, Scalar(hMin, sMin, vMin), Scalar(hMax, sMax, vMax), outIsRangeImage); // Применяем фильтр черно белого диапозона чтобы осталось видно только желтый цвет на светофоре
 
-    Moments m = moments(outIsRangeImage, true);
-
+    Moments m = moments(outIsRangeImage, true); // Определяем координаты центра отфильтрованного обьекта
     double centerX = m.m10 / m.m00, centerY = m.m01 / m.m00;
-    circle(img, Point(centerX, centerY), 10, Scalar(0, 0, 255), -1);
+    circle(img, Point(centerX, centerY), 10, Scalar(0, 0, 255), -1); // Рисуем по центру оркужность радиусом 10
+ 
+    string textImg = "X: " + to_string((int)centerX) + " Y: " + to_string((int)centerY); // Строка для вывода 
+    putText(img, textImg, Point(10, 30), FONT_HERSHEY_SIMPLEX, 0.7, Scalar(0, 0, 0), 2);
 
-    imshow("Окно", img);
+    imshow("Окно", outIsRangeImage); // Отображаем окно
     waitKey();
 }
